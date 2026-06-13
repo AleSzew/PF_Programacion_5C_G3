d1444600b13a6d9802e7789bc76e735c63999f5d
AlejandroSzew <49006858@est.ort.edu.ar>
2026-06-13 11:41:26 -0300

    conexion iphone flutter

---
+++ Prueba componentes/prueba_reactive_ble/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\ale20\Downloads\flutter_windows_3.41.6-stable\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\ale20\OneDrive\Documentos\2026\PF_Programacion_5C_G3\Prueba componentes\prueba_reactive_ble"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=0.1.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
